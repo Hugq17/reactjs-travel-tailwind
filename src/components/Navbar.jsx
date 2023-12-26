8ef71d02e4f3d92b425e732c60d2a95519541276
hung1720 <coderlazy17@gmail.com>
2023-12-26 09:52:47 +0700

    update Hero.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsPerson } from 'react-icons/bs'
 import { BiSearch } from 'react-icons/bi'
 import { AiOutlineClose } from 'react-icons/ai'
@@ -7,10 +7,16 @@ import { FaInstagram, FaPinterest, FaTwitter, FaYoutube, FaFacebook } from 'reac
 
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const [logo, setLogo] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+        setLogo(!logo)
+    }
     return (
-        <div className='flex justify-between items-center h-20 px-4'>
-            <div>
-                <h1>BEACHES.</h1>
+        <div className='flex w-full justify-between items-center h-20 px-4 absolute z-10 text-white'>
+            <div >
+                <h1 onClick={handleNav} className={logo ? 'hidden' : 'block'}>BEACHES.</h1>
             </div>
             {/* can resonsive cho man hinh thi xai sm:md:lg:xl */}
             <ul className='hidden md:flex'>
@@ -21,28 +27,28 @@ const Navbar = () => {
                 <li>Book</li>
             </ul>
             <div className='hidden md:flex'>
-                <BiSearch size={20} />
+                <BiSearch className='mr-2' size={20} />
                 <BsPerson size={20} />
             </div>
-            <div className='md:hidden'>
-                <HiOutlineMenuAlt4 size={20} />
+            <div onClick={handleNav} className='md:hidden z-10'>
+                {nav ? <AiOutlineClose className='text-black' size={20} /> : <HiOutlineMenuAlt4 size={20} />}
             </div>
 
             {/* Mobile menu dropdown */}
             {/* xem them absolute, left, top, w-full, px, py, flex-col */}
-            <div className='absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col'>
+            <div onClick={handleNav} className={nav ? 'absolute left-0 top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col' : 'absolute left-[-100%]'}>
                 <ul>
                     <h1>BEACHES.</h1>
-                    <li>Home</li>
-                    <li>Destinations</li>
-                    <li>Travel</li>
-                    <li>View</li>
-                    <li>Book</li>
-                    <div>
-                        <button>Search</button>
+                    <li className='border-b'>Home</li>
+                    <li className='border-b'>Destinations</li>
+                    <li className='border-b'>Travel</li>
+                    <li className='border-b'>View</li>
+                    <li className='border-b'>Book</li>
+                    <div className='flex flex-col'>
+                        <button className='my-6'>Search</button>
                         <button>Account</button>
                     </div>
-                    <div className=''>
+                    <div className='flex justify-between my-6'>
                         <FaFacebook className='icon' />
                         <FaTwitter className='icon' />
                         <FaYoutube className='icon' />
