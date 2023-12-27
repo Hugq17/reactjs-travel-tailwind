c5eb1f467636d981781205e95f20ec865b8c9c75
hung1720 <coderlazy17@gmail.com>
2023-12-27 12:44:46 +0700

    update test.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
+import Test from "./components/Test";
 
 function App() {
   return (
     <div>
-      <Navbar />
-      <Hero />
+      {/* <Navbar /> */}
+      {/* <Hero /> */}
+      <div className="w-full h-screen flex justify-center items-center bg-pink-300">
+        <Test />
+      </div>
     </div>
   );
 }
---
+++ src/components/Test.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Test = () => {
+    return (
+        <div className='bg-blue-300 w-[1000px] h-[200px] overflow-y-scroll scrollbar-thin scrollbar-thumb-rose-500 scrollbar-track-blue-200'>
+            <div>
+                <div class="sticky top-0 w-full bg-gray-600">Sticky Heading 1</div>
+                <p class="py-4">Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi.</p>
+            </div>
+            <div>
+                <div class="sticky top-0 w-full bg-gray-600">Sticky Heading 2</div>
+                <p class="py-4">Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi.</p>
+            </div>
+            <div>
+                <div class="sticky top-0 w-full bg-gray-600">Sticky Heading 3</div>
+                <p class="py-4">Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi.</p>
+            </div>
+            <div>
+                <div class="sticky top-0 w-full bg-gray-600">Sticky Heading 4</div>
+                <p class="py-4">Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Test
